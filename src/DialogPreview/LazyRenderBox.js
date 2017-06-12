af87281775773c78bf27902200a7877f8b399e13
观通 <lijin.jlj@alipay.com>
2017-06-12 17:21:42 +0800

    tweak code.

--- src/DialogPreview/LazyRenderBox.js
+++ src/DialogPreview/LazyRenderBox.js
@@ -1,4 +1,5 @@
-import React from 'react'  import PropTypes from 'prop-types'
+import React from 'react'
+import PropTypes from 'prop-types'
 import assign from 'object-assign'
 /* eslint-disable */
 const LazyRenderBox = React.createClass({
